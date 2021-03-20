5f0e75a3329b8c4267bda6459c8fd45b2cb1bf63
nitishpatel7 <sonu.patel@daffodilsw.com>
2021-03-20 23:29:34 +0530

    change in code

--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import TabNavigator from './TabNavigator';
-import {Profile,Setting }from '../screens';
-import {View, Image, Text, TouchableOpacity, StyleSheet} from 'react-native';
-import {home, profile, setting} from '../images';
+
+import React from "react";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import TabNavigator from "./TabNavigator";
+import { View, Image, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { home, profile, setting } from "../images";
 
 const Drawer = createDrawerNavigator();
-const renderDrawerItem = ({text, icon, props}) => {
-  let {navigation, state: {routeNames = [], index = 0} = {}} = props;
-  let active = text === routeNames[index];
+const renderDrawerItem = ({ text, icon, props }) => {
+  let { navigation, state: { routes, index } = {} } = props;
+  let { state: { index: activeRouteIndex, routeNames = [] } = {} } = routes[index];
+  let active = text === routeNames[activeRouteIndex];
   return (
     <TouchableOpacity
       onPress={() => navigation.navigate(text)}
-      style={{
-        ...styles.renderDrawerItemStyle,
-        backgroundColor: active ? '#f8f8f8' : 'white',
-      }}>
+      style={{ ...styles.renderDrawerItemStyle, backgroundColor: active ? "#f8f8f8" : "white" }}
+    >
       <Image source={icon} style={styles.imageStyle} />
       <Text style={styles.textStyle}>{text}</Text>
     </TouchableOpacity>
@@ -25,18 +24,17 @@ const DrawerNavigator = () => {
   return (
     <Drawer.Navigator
       drawerContentOptions={{
-        itemStyle: {marginVertical: 5},
+        itemStyle: { marginVertical: 5 },
       }}
-      drawerContent={props => (
-        <View style={{paddingHorizontal: 10, paddingVertical: 15}}>
-          {renderDrawerItem({text: 'Home', icon: home, props})}
-          {renderDrawerItem({text: 'Setting', icon: setting, props})}
-          {renderDrawerItem({text: 'Profile', icon: profile, props})}
+      drawerContent={(props) => (
+        <View style={{ paddingHorizontal: 10, paddingVertical: 15 }}>
+          {renderDrawerItem({ text: "Home", icon: home, props })}
+          {renderDrawerItem({ text: "Setting", icon: setting, props })}
+          {renderDrawerItem({ text: "Profile", icon: profile, props })}
         </View>
-      )}>
-      <Drawer.Screen name="Home" component={TabNavigator} />
-      <Drawer.Screen name="Setting" component={Setting} />
-      <Drawer.Screen name="Profile" component={Profile} />
+      )}
+    >
+      <Drawer.Screen name="TabScreen" component={TabNavigator} />
     </Drawer.Navigator>
   );
 };
@@ -45,12 +43,12 @@ export default DrawerNavigator;
 
 const styles = StyleSheet.create({
   renderDrawerItemStyle: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginBottom: 15,
     paddingVertical: 10,
     paddingHorizontal: 10,
-    borderRadius:5
+    borderRadius: 5,
   },
-  imageStyle: {width: 20, height: 20},
-  textStyle: {paddingLeft: 15},
+  imageStyle: { width: 20, height: 20 },
+  textStyle: { paddingLeft: 15 },
 });
